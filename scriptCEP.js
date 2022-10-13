9b57e7ff6f782e0920208e8071c2009323a67ac1
D. Salvador <disney.js@gmail.com>
2022-10-13 15:30:07 -0300

    Atualização 13OUT22
    Atualização API CEP

--- scriptCEP.js
+++ scriptCEP.js
@@ -12,10 +12,27 @@ fetch(`https://cdn.apicep.com/file/apicep/${cep}.json`)
 })
 .then(res => {
   console.log(res)
-  let p = document.createElement('p')
-  let element = document.createTextNode(`Logradouro: ${res.address} Bairro: ${res.district} Cidade: ${res.city} Estado: ${res.state}`)
-  p.appendChild(element)
-  document.getElementById('result').appendChild(p)
+  let p1 = document.createElement('p')
+  let p2 = document.createElement('p')
+  let p3 = document.createElement('p')
+  let p4 = document.createElement('p')
+
+  let logradouro = document.createTextNode(`Logradouro: ${res.address}`)
+  p1.appendChild(logradouro)
+
+  let bairro = document.createTextNode(`Bairro: ${res.district}`)
+  p2.appendChild(bairro)
+
+  let cidade = document.createTextNode(`Cidade: ${res.city}`)
+  p3.appendChild(cidade)
+
+  let estado = document.createTextNode(`Estado: ${res.state}`)
+  p4.appendChild(estado)
+
+  document.getElementById('result').appendChild(p1)
+  document.getElementById('result').appendChild(p2)
+  document.getElementById('result').appendChild(p3)
+  document.getElementById('result').appendChild(p4)
 })
 .catch(error => {
   document.getElementById('error').appendChild(document.createTextNode(error))
