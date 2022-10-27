d01ef547f7a7f82de7c3c3d4fd95b45f2bd68d05
D. Salvador <disney.js@gmail.com>
2022-10-27 19:43:24 -0300

    27 de outubro de 2022
    Atualização:
    - inserção de imagem de satelite (Clima)
    - Criação script validação de email (Form)
    - Área no formulário para número e complemento

--- scriptClima.js
+++ scriptClima.js
@@ -1,17 +1,21 @@
 /* CLIMA  */
 const btnCid = document.getElementById('btnCid')
    
-btnCid.addEventListener('click', function () {
+btnCid.addEventListener('click', function() {
     clima(document.getElementById('cid').value);
+    mapa();
     document.getElementById("resultp1").innerHTML = "";
     document.getElementById("resultp2").innerHTML = "";
     document.getElementById("resultp3").innerHTML = "";
     document.getElementById("resultp4").innerHTML = "";
+    document.getElementById("resultp5").innerHTML = "";
+    document.getElementById("resultp6").innerHTML = "";
+    document.getElementById("resultp7").innerHTML = "";
     document.getElementById("error").innerHTML = "";
 })
 
 function clima(cid) {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cid},BR&APPID={inserir-key}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cid},BR&APPID=ca7f11d79102681aa6822e5330961dc6&lang=pt_br&units=metric`)
         .then((response) => {
             return response.json()
         })
@@ -21,23 +25,41 @@ function clima(cid) {
             let p2 = document.createElement('p')
             let p3 = document.createElement('p')
             let p4 = document.createElement('p')
+            let p5 = document.createElement('p')
+            let p6 = document.createElement('p')
 
             let situacao = document.createTextNode(` ${res.weather[0].description}`);
             p1.appendChild(situacao);
 
-            let temp = document.createTextNode(` ${res.main.temp}`);
+            let temp = document.createTextNode(`${res.main.temp} ºC`);
             p2.appendChild(temp);
 
-            let umidade = document.createTextNode(` ${res.main.humidity}`)
+            let umidade = document.createTextNode(`${res.main.humidity} %`)
             p3.appendChild(umidade)
 
-            let velocVento = document.createTextNode(` ${res.wind.speed}`)
-            p4.appendChild(velocVento)
+            let velocVento = document.createTextNode(`${Math.round(res.wind.speed*3.6)} km/h`)
+            p4.appendChild(velocVento);
+
+            let nascerSolUTC = (`${res.sys.sunrise}`);
+                dateObj = new Date(nascerSolUTC * 1000);
+                utcString = dateObj.toString();
+            let timeNasc = utcString.slice(16, 24);
+            let nascerSolBr = document.createTextNode(timeNasc);
+            p5.appendChild(nascerSolBr);
+
+            let porSolUTC = (`${res.sys.sunset}`);
+                dateObj = new Date(porSolUTC * 1000);
+                utcString = dateObj.toString();
+            let timePor = utcString.slice(16, 24);
+            let porSolBr = document.createTextNode(timePor);
+            p6.appendChild(porSolBr);
 
             document.getElementById('resultp1').appendChild(p1)
             document.getElementById('resultp2').appendChild(p2)
             document.getElementById('resultp3').appendChild(p3)
             document.getElementById('resultp4').appendChild(p4)
+            document.getElementById('resultp5').appendChild(p5)
+            document.getElementById('resultp6').appendChild(p6)
         })
         .catch(error => {
             document.getElementById('error').appendChild(document.createTextNode("Algum erro aconteceu. Refaça sua busca. Digite somente o nome da cidade."))
